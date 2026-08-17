4dbfb24e869bfec5cfaa8a069800525e4cec8516
imshubhamgiri <skgiri569@gmail.com>
2026-08-17 14:58:41 +0530

    feat: setup redis pub sub and export io

--- server/src/config/socket.config.ts
+++ server/src/config/socket.config.ts
@@ -1,6 +1,6 @@
 import { Server as HTTPServer } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
-
+import {redisSubscriber} from '../config/redis.config.js'
 
 
 
@@ -15,24 +15,54 @@ export const initSocket = (server: HTTPServer): SocketIOServer => {
           credentials: true
         }
       });
-    io.on('connection', (socket) => {
-        console.log(`Socket connected: ${socket.id}`);
 
-        socket.on('join_room', (roomId: string) => {
-            socket.join(roomId);
-            console.log(`Socket ${socket.id} joined room: ${roomId}`);
-        });
+      io.on('connection', (socket) => {
+          console.log(`Socket connected: ${socket.id}`);
+  
+          socket.on('join_room', (roomId: string) => {
+              socket.join(roomId);
+              console.log(`Socket ${socket.id} joined room: ${roomId}`);
+          });
+  
+          socket.on('disconnect', () => {
+              console.log(`Socket disconnected: ${socket.id}`);
+          })
+  
+          socket.on('scrape_status', (data) => {
+              console.log(`Received scrape status from ${socket.id}:`, data);
+          })
+          socket.on('connect', (id) => {
+             console.log(`Socket connected: ${socket.id}`, id);
+          })
+      });
 
-        socket.on('disconnect', () => {
-            console.log(`Socket disconnected: ${socket.id}`);
-        })
+      redisSubscriber.subscribe('scrape_status', (err, count) => {
+        if(err){
+            console.error('Failed to subscribe: ', err.message);
+        }
+      })
 
-        socket.on('scrape_status', (data) => {
-            console.log(`Received scrape status from ${socket.id}:`, data);
-        })
-        socket.on('connect', (id) => {
-           console.log(`Socket connected: ${socket.id}`, id);
-        })
-    });
+      redisSubscriber.on('message', (channel, message) => {
+        if (channel === 'scrape_status_channel') {
+            try {
+              const data = JSON.parse(message);
+              if (data.roomId) {
+                io!.to(data.roomId).emit('scrape_status', data);
+              }
+            } catch (err) {
+              console.error('Error parsing Redis pub/sub message:', err);
+            }
+          }
+      })
+
+
     return io;
 };
+
+
+export const getIO = (): SocketIOServer => {
+    if (!io) {
+        throw new Error("Socket.io not initialized. Call initSocket first.");
+    }
+    return io;
+}
