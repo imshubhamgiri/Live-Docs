6c60091b71ac14c9d518a43ee95c3c18deb4db16
imshubhamgiri <skgiri569@gmail.com>
2026-08-20 22:47:19 +0530

    implement ratelimiter to routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
+import { chatLimiter,scrapeLimiter } from '../middleware/rate.limiter.js';
 import chatRoutes from './chat.routes.js';
 import scrapeRoutes from './scrape.routes.js';
 import { Router } from 'express';
 
 const router = Router();
 
-router.use('/v1/scrape', scrapeRoutes);
-router.use('/v1/chat', chatRoutes);
+router.use('/v1/scrape',scrapeLimiter ,scrapeRoutes);
+router.use('/v1/chat',chatLimiter ,chatRoutes);
 
 export default router;
